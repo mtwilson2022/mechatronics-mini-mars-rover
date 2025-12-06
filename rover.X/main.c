c9ebc99ca2a2f27760c47a2372482383863d6e83
reisi <reisi@SLIM7.app.byu.edu>
2025-12-06 15:09:55 -0700

    Working code for everything except data transmission module. Having issues detecting the line to turn into the lander pad.

--- rover.X/main.c
+++ rover.X/main.c
@@ -20,7 +20,8 @@
 RobotTaskState robotTaskState = LINE_FOLLOW;
 CanyonSensorState canyonSensorState = STRAIGHT;
 LineSensorState lineSensorState = CENTER;
-
+int sampleReturned = 0;
+int sampleCollected = 0;
 
 int main(int argc, char** argv) {
     _RCDIV = 0b100; // oscillator: divide by 16 postscaling (so Fosc = 500 kHz)
@@ -29,14 +30,20 @@ int main(int argc, char** argv) {
     setupMotors();
     configAD();
     
-    
-
+//////////////start code (hardcoded for start)/////////
+    while(!(FRONT_SHARP_SIG > FRONT_SHARP_THRESH_START)) {
+        continue;
+    }
+    goStraight(QUARTER_SPEED);
+    moveForward(800);
+    turnLeftGetOnLine();
+///////////////////////////////////////////////////////
     
     while (1) {
         switch (robotTaskState) {
             case LINE_FOLLOW:
                 
-                lineNav();
+                lineNav();                
                 
                 if (lineSensorState == NO_ACTIVE) {
                     if (checkOffLine()){
@@ -45,27 +52,32 @@ int main(int argc, char** argv) {
                 }
 
                 
-//                if (SAMPLE_IR_SIG > IR_SIG_THRESH) {
-//                    robotTaskState = SAMPLE_COLLECT;
-//                    goStraight(FULL_SPEED);
-//                } 
+                if ((lineSensorState == CENTER) && (SAMPLE_IR_SIG > IR_SIG_THRESH)) {
+                        robotTaskState = SAMPLE_COLLECT;
+                } 
+//                
+                if ((lineSensorState == CENTER) && (RIGHT_SHARP_SIG > RIGHT_SHARP_THRESH_SAMPLE_RETURN)) {
+                    if ((sampleCollected == 1) && (sampleReturned == 0)) {
+                        robotTaskState = SAMPLE_RETURN;
+                    }
+                }
                 
-//                if ((RIGHT_SONAR_SIG < SONAR_THRESHOLD) && (CENTER_LINE_SIG < LINE_SENSOR_THRESHOLD)) {
-//                    robotTaskState = SAMPLE_RETURN;
-//                    goStraight(FULL_SPEED);
-//                }
-                
-                // TODO: add transition to DATA_TRANSMIT
-//                if ((FAR_LEFT_LINE_SIG < LINE_SENSOR_THRESHOLD) && (CENTER_LINE_SIG < LINE_SENSOR_THRESHOLD)
-//                        && (LEFT_LINE_SIG < LINE_SENSOR_THRESHOLD) && (RIGHT_LINE_SIG < LINE_SENSOR_THRESHOLD)) {
-//                    if (sampleReturned){
-//                        robotTaskState = DATA_TRANSMIT;
-//                    }
-//                    else {
-//                        turnLeftGetOnLine();
-//                    }
-//                }
-                
+////////////////////////Code to start data transmission module/////////////////////////
+/////////////////////// Currently having issues detecting line to turn into lander!                
+                if ((sampleReturned == 0) && (FAR_LEFT_LINE_SIG < LINE_SENSOR_THRESHOLD) && (CENTER_LINE_SIG < LINE_SENSOR_THRESHOLD) 
+                        && (LEFT_LINE_SIG < LINE_SENSOR_THRESHOLD)) {
+                    stopMotors();
+                    delay(5000);
+                    if ((FAR_LEFT_LINE_SIG < LINE_SENSOR_THRESHOLD) && (CENTER_LINE_SIG < LINE_SENSOR_THRESHOLD) 
+                        && (LEFT_LINE_SIG < LINE_SENSOR_THRESHOLD)) {
+                        robotTaskState = DATA_TRANSMIT;
+                    }
+                    else{
+                        startMotors();
+                    }
+                }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
                 break;
                 
                 
@@ -84,12 +96,9 @@ int main(int argc, char** argv) {
                 
             case SAMPLE_COLLECT:
                 stopMotors();
-                delay(10000);
                 collectSample();
-                
-                // have the robot back up and get on the line, then:
                 robotTaskState = LINE_FOLLOW;
-                
+                sampleCollected = 1;
                 break;
                 
                 
@@ -102,23 +111,20 @@ int main(int argc, char** argv) {
                 else {
                     depositBlackBall();
                 }
-                
-                delay(5000); // instead: turn right/left until on line
-                
-                // is the robot reliably on the line at this point?
+                delay(5000); 
                 robotTaskState = LINE_FOLLOW;
-                
+                sampleReturned = 1;
                 break;
                 
                 
             case DATA_TRANSMIT:
+                goStraight(QUARTER_SPEED);  //sets speed
                 stopMotors();
-                OC2RS = PERIOD * 4;
-                OC3RS = PERIOD * 4;
                 turnLeftGetOnLine();
                 while (!(Collision())){
                     lineNav();
                 }
+                stopMotors();
                 pointLaser();
                 break;
         }
